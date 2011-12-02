d7b6fbf7146b2ad4e68f6cd683b82ddf26f3c9d8
Ankit Bansal <Ankit@AnkitMacGgn.local>
2011-12-02 20:34:41 +0530

    Created base test for Daily Average Csv Downloader

--- src/main/java/com/sa/mt/options/downloader/DailyAverageCsvDownloader.java
+++ src/main/java/com/sa/mt/options/downloader/DailyAverageCsvDownloader.java
@@ -1,5 +1,30 @@
 package com.sa.mt.options.downloader;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Service
 public class DailyAverageCsvDownloader {
-    
+
+    @Value(value = "${daily.average.download.url}")
+    private String downloadFrom;
+
+    @Value(value = "${daily.average.store.path}")
+    private String downloadTo;
+
+    public void download() {
+         HttpURLConnection connection = connection(downloadFrom);
+    }
+
+    private HttpURLConnection connection(String downloadUrl) {
+        try {
+            return (HttpURLConnection) new URL(downloadUrl).openConnection();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/sa/mt/downloader/DailyAverageCsvDownloaderTest.java
+++ src/test/java/com/sa/mt/downloader/DailyAverageCsvDownloaderTest.java
@@ -1,5 +1,24 @@
 package com.sa.mt.downloader;
 
+import com.sa.mt.options.downloader.DailyAverageCsvDownloader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/resources/spring-config.xml"})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class})
 public class DailyAverageCsvDownloaderTest {
 
+    @Autowired
+    private DailyAverageCsvDownloader downloader;
+
+     @Test
+      public void shouldDownloadCsvFile() {
+         downloader.download();
+      }
 }
