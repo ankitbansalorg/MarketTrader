44e46c0874d4f0add14bfafb6c209080a3651412
Ankit Bansal <Ankit@AnkitMacGgn.local>
2011-12-09 21:47:02 +0530

    Done with writing tests for instrument pipeline

--- src/main/java/com/sa/mt/options/pipeline/InstrumentPipeline.java
+++ src/main/java/com/sa/mt/options/pipeline/InstrumentPipeline.java
@@ -4,24 +4,29 @@ import com.sa.mt.options.domain.Instrument;
 import com.sa.mt.options.loader.InstrumentLoader;
 import com.sa.mt.options.parser.InstrumentCsvParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.List;
 
 @Service
-public class InstrumentFileServer {
+public class InstrumentPipeline {
     private InstrumentCsvParser instrumentCsvParser;
     private InstrumentLoader instrumentLoader;
 
+
+    @Value(value = "${instrument.store.url}")
+    private String storageUrl;
+
     @Autowired
-    public InstrumentFileServer(InstrumentCsvParser instrumentCsvParser, InstrumentLoader instrumentLoader) {
+    public InstrumentPipeline(InstrumentCsvParser instrumentCsvParser, InstrumentLoader instrumentLoader) {
         this.instrumentCsvParser = instrumentCsvParser;
         this.instrumentLoader = instrumentLoader;
     }
 
     public void storeData(String storageUrl) {
-               String[] fileNames = new File(storageUrl).list();
+        String[] fileNames = new File(storageUrl).list();
         for(String fileName : fileNames) {
             if(fileName.endsWith(".csv")) {
                 File parsableFile = new File(storageUrl + fileName);
@@ -31,7 +36,7 @@ public class InstrumentFileServer {
         }
     }
 
-       private void storeFileData(File file) {
+    private void storeFileData(File file) {
         List<Instrument> instruments =  instrumentCsvParser.parse(file);
         instrumentLoader.loadData(instruments);
     }
---
+++ src/test/java/com/sa/mt/options/pipeline/InstrumentPipelineTest.java
@@ -0,0 +1,48 @@
+package com.sa.mt.options.pipeline;
+
+import com.sa.mt.options.domain.Instrument;
+import com.sa.mt.options.loader.InstrumentLoader;
+import com.sa.mt.options.parser.InstrumentCsvParser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.File;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyListOf;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(InstrumentPipeline.class)
+public class InstrumentPipelineTest {
+
+    @Mock
+    private InstrumentLoader instrumentLoader;
+    @Mock
+    private InstrumentCsvParser instrumentCsvParser;
+
+    private InstrumentPipeline pipeline;
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        pipeline = new InstrumentPipeline(instrumentCsvParser, instrumentLoader);
+    }
+
+    @Test
+      public void shouldStoreData() throws Exception {
+        File mockFile = mock(File.class);
+        whenNew(File.class).withArguments(anyString()).thenReturn(mockFile);
+        when(mockFile.list()).thenReturn(new String[] {"File1.csv", "File2.csv"});
+        pipeline.storeData("some url");
+        verify(instrumentCsvParser, times(2)).parse(any(File.class));
+        verify(instrumentLoader, times(2)).loadData(anyListOf(Instrument.class));
+      }
+}
